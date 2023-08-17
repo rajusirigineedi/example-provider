fb9cd0d069f374120f8138a21d3156d807d20e63
Sirigineedi Hemanth Pallapa Raju <sirigineedi@geekyants.com>
2023-08-17 14:53:41 +0530

    some fix

--- src/product/product.js
+++ src/product/product.js
@@ -1,11 +1,10 @@
 class Product {
-  constructor(id, type, name, version, color, duplicate) {
+  constructor(id, type, name, version, color) {
     this.id = id;
     this.type = type;
     this.name = name;
     this.version = version;
     this.color = color;
-    this.duplicate = duplicate;
   }
 }
 
--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -3,15 +3,9 @@ const Product = require("./product");
 class ProductRepository {
   constructor() {
     this.products = new Map([
-      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green", true)],
-      [
-        "10",
-        new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue", true),
-      ],
-      [
-        "11",
-        new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "yellow", true),
-      ],
+      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green")],
+      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")],
+      ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "yellow")],
     ]);
   }
 
