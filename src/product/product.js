d9cc8a4b3661435698eb7558c4c7198ffb7b0a2e
Sirigineedi Hemanth Pallapa Raju <sirigineedi@geekyants.com>
2023-08-17 09:45:33 +0530

    add dup lkey

--- src/product/product.js
+++ src/product/product.js
@@ -1,10 +1,11 @@
 class Product {
-  constructor(id, type, name, version, color) {
+  constructor(id, type, name, version, color, duplicate) {
     this.id = id;
     this.type = type;
     this.name = name;
     this.version = version;
     this.color = color;
+    this.duplicate = duplicate;
   }
 }
 
--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -3,9 +3,15 @@ const Product = require("./product");
 class ProductRepository {
   constructor() {
     this.products = new Map([
-      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green")],
-      ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue")],
-      ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "yellow")],
+      ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green", true)],
+      [
+        "10",
+        new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "blue", true),
+      ],
+      [
+        "11",
+        new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "yellow", true),
+      ],
     ]);
   }
 
